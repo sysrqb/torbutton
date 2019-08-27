0928f38e17224cf67d3059d1656f41a89f142450
Alex Catarineu <acat@torproject.org>
2019-08-27 12:10:10 +0200

    Bug 31520: Remove monthly giving banner from Tor Browser
    This reverts commit 99210412d9f7b33ed40f9407e179c6cbeaa61957.

--- src/chrome/content/aboutTor/aboutTor-content.js
+++ src/chrome/content/aboutTor/aboutTor-content.js
@@ -24,7 +24,6 @@ let { bindPrefAndInit, show_torbrowser_manual } = Cu.import("resource://torbutto
 var AboutTorListener = {
   kAboutTorLoadedMessage: "AboutTor:Loaded",
   kAboutTorChromeDataMessage: "AboutTor:ChromeData",
-  kAboutTorHideDonationBanner: "AboutTor:HideDonationBanner",
 
   get isAboutTor() {
     return content.document.documentURI.toLowerCase() == "about:tor";
@@ -59,22 +58,6 @@ var AboutTorListener = {
     }
   },
 
-  setupBannerClosing: function () {
-    let that = this;
-    let closer = content.document.getElementById("donation-banner-closer");
-    closer.addEventListener("click", function () {
-      sendAsyncMessage(that.kAboutTorHideDonationBanner);
-    });
-
-    bindPrefAndInit("extensions.torbutton.donation_banner_countdown3",
-                    countdown => {
-                      if (content.document && content.document.body) {
-                        content.document.body.setAttribute(
-                          "show-donation-banner", countdown > 0);
-                      }
-                    });
-  },
-
   onPageLoad: function() {
     // Arrange to update localized text and links.
     bindPrefAndInit("intl.locale.requested", aNewVal => {
@@ -82,8 +65,6 @@ var AboutTorListener = {
         this.onLocaleChange(aNewVal);
       }
     });
-
-    this.setupBannerClosing();
 
     // Add message and event listeners.
     addMessageListener(this.kAboutTorChromeDataMessage, this);
